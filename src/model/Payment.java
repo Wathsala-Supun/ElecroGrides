9e2729bdceefec7c7d5a19d981f52babca2c5569
supunchamod <supunchamod3883sc@gmail.com>
2022-04-25 06:09:33 -0700

    connect database

--- src/model/Payment.java
+++ src/model/Payment.java
@@ -1,5 +1,12 @@
 package model;
 
 public class Payment {
+	
+	//connect database
+	private static String url = "jdbc:mysql://localhost:3306/paf";
+	private static String userName = "root";
+	private static String password = " ";
+	
+	
 
 }
